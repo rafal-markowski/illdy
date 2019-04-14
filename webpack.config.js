ac145c9315a75bc216c10c4cbb87f0217089c9c0
rafal <rafal_markowski@onet.pl>
2019-04-14 10:25:32 +0200

    Ustawienie statycznej ścieżki dla serwera developerskiego

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,13 @@ module.exports = {
     ],
     devServer: {
         host: '0.0.0.0',
-        port: 8080
+        port: 8080,
+        proxy: {
+            '/static': {
+                target: 'http://localhost:8080',
+                pathRewrite: {'^/static' : '/src'}
+            }
+        }
     },
     mode: devMode ? 'development' : 'production'
 };
