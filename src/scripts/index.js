ffa158fcde37beb8d37e6c35e79a3e823fad4870
rafal <rafal_markowski@onet.pl>
2019-04-15 20:57:25 +0200

    Stworzenie sekcji Project

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,9 @@ import '../styles/main.scss';
 
 import '../images/text-logo.png';
 import '../images/page-header-bg.jpg';
+import '../images/project_1.jpg';
+import '../images/project_2.jpg';
+import '../images/project_3.jpg';
+import '../images/project_4.jpg';
 
 import './nav';
