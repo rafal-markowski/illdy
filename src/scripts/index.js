7389426f3431176f83124b609b664934f750feef
rafal <rafal_markowski@onet.pl>
2019-04-17 22:18:49 +0200

    Stworzenie sekcji Testimotals

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,5 +6,9 @@ import '../images/project_1.jpg';
 import '../images/project_2.jpg';
 import '../images/project_3.jpg';
 import '../images/project_4.jpg';
+import '../images/overlay_1.jpg';
+import '../images/face_1.jpg';
+import '../images/face_2.jpg';
+import '../images/face_3.jpg';
 
 import './nav';
